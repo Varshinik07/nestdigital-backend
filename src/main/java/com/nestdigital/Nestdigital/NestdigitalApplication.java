ba03cd83de95d1ad776b1372a36aa5f20fe82938
Varshini <varshuk01@gmail.com>
2022-10-27 10:22:05 +0530

    message

---
+++ src/main/java/com/nestdigital/Nestdigital/NestdigitalApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.Nestdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestdigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestdigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/Nestdigital/NestdigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.Nestdigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestdigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
