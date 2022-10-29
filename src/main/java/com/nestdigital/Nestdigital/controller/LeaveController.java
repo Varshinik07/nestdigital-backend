6d673faac5f64d079abbbc0c8dc58c83de6b4cbf
Varshini <varshuk01@gmail.com>
2022-10-29 18:46:39 +0530

    insert-api

--- src/main/java/com/nestdigital/Nestdigital/controller/LeaveController.java
+++ src/main/java/com/nestdigital/Nestdigital/controller/LeaveController.java
@@ -3,13 +3,17 @@ package com.nestdigital.Nestdigital.controller;
 import com.nestdigital.Nestdigital.dao.LeaveDao;
 import com.nestdigital.Nestdigital.model.LeaveModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class LeaveController {
@@ -27,6 +31,25 @@ public class LeaveController {
         System.out.println(leavetable.toString());
         dao.save(leavetable);
         return "{status:'success'}";
+    }
+    @Transactional
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/updatestatus",consumes = "application/json",produces = "application/json")
+    public String updateStatus(@RequestBody LeaveModel leave){
+        dao.updateById(leave.getId());
+        return "{status:'success'}";
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/viewallleaves")
+    public List<Map<String ,String>> viewallleaves(){
+        return (List<Map<String, String>>) dao.viewAllLeaveBy();
+
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/viewleavesbyempid",consumes = "application/json",produces = "application/json")
+    public  List<Map<String,String>> viewLeavesById(@RequestBody LeaveModel leave){
+        return (List<Map<String, String>>) dao.viewLeaveById(leave.getEmpid());
     }
 
 }
--- src/main/java/com/nestdigital/Nestdigital/dao/LeaveDao.java
+++ src/main/java/com/nestdigital/Nestdigital/dao/LeaveDao.java
@@ -2,6 +2,20 @@ package com.nestdigital.Nestdigital.dao;
 
 import com.nestdigital.Nestdigital.model.LeaveModel;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+
+import java.util.List;
+import java.util.Map;
 public interface LeaveDao extends CrudRepository<LeaveModel,Integer> {
+    @Modifying
+    @Query(value = "UPDATE `leave` SET  WHERE `id`= :id",nativeQuery = true)
+    void updateById(Integer id);
+
+    @Query(value = "SELECT `id`, l.`empid`, l.`leave_applydate`, l.`leave_duratiom`, l.`leave_reason`,l.`leave_requireddate`, l.`name`,e.name FROM `leavetable`  l JOIN employee e ON l.empid=e.empcode",nativeQuery = true)
+    List<Map<String,String>>viewAllLeaveBy();
+
+    @Query(value = "SELECT `id`, l.`empid`, l.`leave_applydate`, l.`leave_duratiom`, l.`leave_reason`,l.`leave_requireddate`, l.`name`,e.name FROM `leavetable`  l JOIN employee e ON l.empid=e.empcode WHERE l.empid:empid",nativeQuery = true)
+    List<Map<String,String>>viewLeaveById(Integer emp_id);
 }
